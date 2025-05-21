4d0306ca0a534cb70163247254136cd7c70f9530
Sameer Jadhav <sammyjadhav6699@gmail.com>
2025-05-22 00:25:54 +0530

    Added Landing Page Code

--- app/config/settings.py
+++ app/config/settings.py
@@ -40,7 +40,6 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", 'True').lower() in ['true', 'yes', '1']
 
-
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
 
